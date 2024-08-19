b06c556f62a6df6e16a80b85552b5e36a425b1d6
kaantoo <kaan.yuksel1@gazi.edu.tr>
2024-08-19 07:59:40 +0300

    x

--- ApiHandler.py
+++ ApiHandler.py
@@ -21,8 +21,14 @@ def put_item():
 
 @app.route('/dev/picus/get/<key>', methods=['GET'])
 def get_item(key):
-    response = table.get_item(Key={'id': key})
-    return jsonify(response['Item'])
+    try:
+        response = table.get_item(Key={'id': key})
+        if 'Item' in response:
+            return jsonify(response['Item'])
+        else:
+            return jsonify({'error': 'Item not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port = 8082, debug=True)
+    app.run(host='127.0.0.1', port=8082, debug=True)
