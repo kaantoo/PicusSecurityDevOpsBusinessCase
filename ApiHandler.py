20e0388fe089e55669e41271046295d10ac9b791
kaantoo <kaan.yuksel1@gazi.edu.tr>
2024-08-19 07:24:12 +0300

    o

--- ApiHandler.py
+++ ApiHandler.py
@@ -25,4 +25,4 @@ def get_item(key):
     return jsonify(response['Item'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000, debug=True)
+    app.run(host='127.0.0.1', port = 8082, debug=True)
